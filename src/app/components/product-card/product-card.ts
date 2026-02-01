b7c0708fe55c83b1b28db0152c84b88e47f67ff0
Бахыт Алтаев <bahytaltaev@Bahyts-MacBook-Air.local>
2026-02-02 02:21:14 +0500

    fast commit

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,17 +1,25 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
 import { Product } from '../../models/product';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { EcommerceStore } from '../../ecommerce-store';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatButton, MatIcon],
+  imports: [MatButton, MatIcon, MatIconButton],
   template: `
-    <div class="bg-white cursor-pointer rounded-xl shadow-lg overflow-hidden flex flex-col h-full">
+    <div class="relative bg-white cursor-pointer rounded-xl shadow-lg overflow-hidden flex flex-col h-full">
       
       <img [src]="product().imageUrl"
            class="w-full h-[300px] object-cover rounded-t-xl" />
+
+           <button class="!absolute z-10 top-3 right-3 w-10 h-10 rounded-full !bg-white border-0 shadow-md flex items-center justify-center cursor-pointer transition-all duration-200 hover:scale-110 hover:ahadow-lg"
+          matIconButton
+          (click)="toggleWishlist(product())"
+           >
+           <mat-icon>favorite</mat-icon>
+          </button>
 
       <div class="p-5 flex flex-col flex-1">
 
@@ -41,5 +49,16 @@ import { MatIcon } from '@angular/material/icon';
   styles: ``,
 })
 export class ProductCard {
-  product = input.required<Product>();
+product = input.required<Product>();
+  addToCartClicked=output<Product>();
+  store=inject(EcommerceStore)
+  isInWishlist=computed(() => this.store.wishlistItems().find(p=>p.id ===this.product().id))
+  toggleWishlist(product: Product) {
+if(this.isInWishlist()){
+// remove this 
+} else{
+
+  this.store.addToWishlist(product);
 }
+  }
+} 
