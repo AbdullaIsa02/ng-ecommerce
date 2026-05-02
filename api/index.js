eced55da5247a45d30069575df737a2a7e590ac8
Бахыт Алтаев <bahytaltaev@Bahyts-Air.lan>
2026-05-02 22:33:09 +0500

    Fix SSR for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-import * as mod from '../dist/ng-ecommerce/server/server.mjs';
+import handler from '../dist/ng-ecommerce/server/server.mjs';
 
-export default mod.default || mod.app || mod.handler;
+export default handler;
