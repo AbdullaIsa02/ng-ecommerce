fbc00813a2855aff58dd6e38136652d88612cb46
Бахыт Алтаев <bahytaltaev@Bahyts-Air.lan>
2026-05-02 23:37:03 +0500

    fix default export lazy load

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-import handler from '../dist/ng-ecommerce/server/server.mjs';
-
-export default handler;
+export default async (req, res)=>{
+const {reqHandler} =await import('../dist/ng-ecommerce/server/server.mjs');
+return reqHandler(req,res);
+};
