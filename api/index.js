7c1abc587f7888e2f66c83f4c60e8bf370254fda
Бахыт Алтаев <bahytaltaev@Bahyts-Air.lan>
2026-05-03 11:47:22 +0500

    fix SSR build for vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
-export default async (req, res) => {
-  const { default: handler } = await import('../dist/ng-ecommerce/server/server.mjs');
-  return handler(req, res);
-};
+export default async function handler(req, res) {
+  const mod = await import('../dist/ng-ecommerce/server/server.mjs');
+
+  const fn =
+    mod.default ||
+    mod.app ||
+    mod.handler ||
+    mod.reqHandler;
+
+  if (!fn) {
+    return res.status(500).send('SSR handler not found');
+  }
+
+  return fn(req, res);
+}
